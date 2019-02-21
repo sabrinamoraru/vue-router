38b5df2f7aaf97972f48f4f30eeb652a39d77935
Sabrina Moraru <sabrina.moraru@gmail.com>
2019-02-21 20:27:51 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>The Home Page</h1>
+    <hr>
+    <li>setting up routes</li>
+    <li>navigating</li>
+    <li>passing parameters</li>
+    <li>reacting to parameter changes</li>
+    <li>passing query parameters</li>
+    <li>scrolling</li>
+    <li>loading components</li>
+    <li>lazy loading</li>
+    <li>protecting routes with guards</li>
+    <h3>Useful Links:</h3>
+    <br>
+    <li>
+      <a href="https://github.com/vuejs/vue-router" target="_blank">vue-router: Github Page</a>
+    </li>
+    <br>
+    <li>
+      <a href="https://router.vuejs.org/en/" target="_blank">vue-router: Documentation</a>
+    </li>
+  </div>
+</template>0
